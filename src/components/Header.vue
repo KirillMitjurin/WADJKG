52b9e4826f7bcb21fb80e489d65a5e5daaa4f838
Jüri Tsõmbaljuk <juri.tsombaljuk@gmail.com>
2024-11-24 00:39:43 +0200

    the project went to sh*t, but maybe vue works?????

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header">
+      <ul class="nav">
+        <li><a href="index.html">Home</a></li>
+        <li id="divider"><a href="addPost.html">Add Post</a></li>
+      </ul>
+      <div class="profile-menu">
+        <a class="logo">
+          <img src="res/images/user.png" width="50" height="50" alt="User picture" class="user-photo" />
+        </a>
+        <div class="dropdown">
+          <div class="dropdown-content">
+            <p>John Doe</p>
+            <p>john.doe@ut.ee</p>
+            <a href="login.html">Logout</a>
+          </div>
+        </div>
+      </div>
+    </header>
+  </template>
+  
---
+++ src/components/Post.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="post">
+      <div class="post-header">
+        <img :src="post.image" class="user-pic" alt="User" />
+        <div class="post-date">{{ post.date }}</div>
+      </div>
+      <img v-if="post.image" :src="post.image" class="post-image" alt="Post image" />
+      <div class="post-text">{{ post.text }}</div>
+      <div class="post-footer">
+        <img src="res/images/thumbsUp.png" class="thumbs-up" alt="Thumbs Up" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['post'],
+  };
+  </script>
+  

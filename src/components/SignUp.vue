492d8d0827c9841304463405f2dc2791a03291ab
loorand <georg.loorand@gmail.com>
2024-11-24 17:01:13 +0200

    Add password conditions to Signup, adjust like button border

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -25,32 +25,72 @@
             placeholder="Password"
             required
           />
+          <p v-if="passwordError && password" class="error-message">{{ passwordError }}</p>
         </div>
-        <button type="submit">Sign Up</button>
+        <button type="submit" :disabled="isPasswordInvalid">Sign Up</button>
       </form>
     </div>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        email: "",
-        username: "",
-        password: "",
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+      passwordError: "",
+    };
+  },
+  computed: {
+    isPasswordInvalid() {
+      return !this.isPasswordValid;
+    },
+    isPasswordValid() {
+      const lengthCheck = this.password.length >= 8 && this.password.length <= 15;
+      const uppercaseCheck = /[A-Z]/.test(this.password);
+      const lowercaseCheck = /[a-z].*[a-z]/.test(this.password);
+      const numericCheck = /\d/.test(this.password);
+      const startUppercaseCheck = /^[A-Z]/.test(this.password);
+      const underscoreCheck = /_/.test(this.password);
+
+      if (!lengthCheck) {
+      this.passwordError = "The password should be between 8 and 15 characters long.";
+    } else if (!uppercaseCheck) {
+      this.passwordError = "The password should include at least one uppercase alphabet character.";
+    } else if (!lowercaseCheck) {
+      this.passwordError = "The password should include at least two lowercase alphabet characters.";
+    } else if (!numericCheck) {
+      this.passwordError = "The password should include at least one numeric value.";
+    } else if (!startUppercaseCheck) {
+      this.passwordError = "The password should start with an uppercase letter.";
+    } else if (!underscoreCheck) {
+      this.passwordError = "The password should include an underscore.";
+    } else {
+      this.passwordError = ""; // Password is valid, clear error message
+    }
+
+    return lengthCheck && uppercaseCheck && lowercaseCheck && numericCheck && startUppercaseCheck && underscoreCheck;
+  
+    }
+  },
+  methods: {
+    handleSignUp() {
+      const newUser = {
+        email: this.email,
+        username: this.username,
+        password: this.password,
       };
+      // Emit user data to the parent component for further processing
+      this.$emit("signUp", newUser);
     },
-    methods: {
-      handleSignUp() {
-        const newUser = {
-          email: this.email,
-          username: this.username,
-          password: this.password,
-        };
-        // Emit user data to the parent component for further processing
-        this.$emit("signUp", newUser);
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
+
+<style>
+  .error-message {
+    color: red;
+  }
+</style>
   

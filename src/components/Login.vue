b5eb2d2f664512f8fc004178a02ba8c1ff6211c8
loorand <georg.loorand@gmail.com>
2024-11-24 14:45:22 +0200

    ?

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1>Login Page</h1>
+      <form @submit.prevent="handleLogin">
+        <div>
+          <input type="text" v-model="username" placeholder="Username" required />
+        </div>
+        <div>
+          <input type="password" v-model="password" placeholder="Password" required />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      handleLogin() {
+        console.log('Logging in:', this.username);
+        // Example logic for login
+      },
+    },
+  };
+  </script>
+  
